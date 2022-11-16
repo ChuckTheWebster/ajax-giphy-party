3ed823f1883ae31c4c5be1655e04665f0bef9a8c
Youssef Doss <youssefdoss97@gmail.com>
2022-11-15 19:42:52 -0500

    add gifs to page

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("Let's get this party started!");
+'use strict';
+
+function getSearchValueAndSearch(evt) {
+    evt.preventDefault();
+    let searchTerm = $('#search').val();
+    getGif(searchTerm);
+}
+
+async function getGif(searchTerm) {
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=ifGZaDq48hprw2OFYlDXvOMKnb79pL6X`);
+    console.log(response);
+    addGifToPage(response);
+}
+
+function addGifToPage(response) {
+    let gifSource = response.data.data[0].images.original.url;
+    console.log(response.data.data[0].images.original.url);
+    let $newGif = $(`<img src="${gifSource}">`);
+    $('body').append($newGif);
+}
+
+$('form').on('submit', getSearchValueAndSearch);
